7de4d024baf3233e15dbb645cc943d70430c24e1
Mr Pie <chappiewuzefan@gmail.com>
2026-04-08 16:12:31 +1000

    Remove homepage skip link

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -73,12 +73,6 @@ export default async function LocalePage({ params }: LocalePageProps) {
   return (
     <>
       <JsonLd data={structuredData} />
-      <a
-        href="#main-content"
-        className="sr-only focus:not-sr-only focus:fixed focus:left-4 focus:top-4 focus:z-50 focus:rounded-full focus:bg-white focus:px-4 focus:py-2 focus:text-slate-950"
-      >
-        {content.common.skipToMain}
-      </a>
       {showPlaceholderWarning ? (
         <LaunchWarning message={content.common.placeholderBanner} />
       ) : null}
